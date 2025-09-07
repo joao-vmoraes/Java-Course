c9d886f1740d7c61f51e701da0deaa51d1e9d8b6
João Moraes <jvmmoraesdev@gmail.com>
2025-09-07 01:27:43 -0300

    Add JavaDoc

--- src/MaratonaJava/Introducao/OlaMundo.java
+++ src/MaratonaJava/Introducao/OlaMundo.java
@@ -1,5 +1,9 @@
 package MaratonaJava.Introducao;
 
+
+/**
+ * This is a <b>JavaDoc</b> Message
+ */
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Ola mundo");
