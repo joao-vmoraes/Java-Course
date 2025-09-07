c678d64cec8eb1bb87f65cf40a1ebc02b2d8c75f
João Moraes <jvmmoraesdev@gmail.com>
2025-09-07 11:02:32 -0300

    Add Operadores

---
+++ src/MaratonaJava/Introducao/Operadores.java
@@ -0,0 +1,22 @@
+package MaratonaJava.Introducao;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // + - * /
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num2 - num1);
+        System.out.println(num2 + num1);
+        System.out.println(num2 / num1);
+        System.out.println(num2 * num1);
+
+        // % (resto), < , > , >= , <= , !=
+
+        boolean isDezMaiorQueVinte = num1 > num2;
+        System.out.println(isDezMaiorQueVinte);
+
+        // && (AND) , || (OR) , ! (NOT)
+
+        // += , -+ , ++ , --
+    }
+}
