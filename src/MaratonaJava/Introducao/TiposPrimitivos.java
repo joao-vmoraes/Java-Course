593ae6d9f41a30d231ac17d2842e07a8004c7b78
João Moraes <jvmmoraesdev@gmail.com>
2025-09-07 10:31:43 -0300

    Add TiposPrimitivos

---
+++ src/MaratonaJava/Introducao/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package MaratonaJava.Introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, dlouble, float, char, byte, short, long, boolean
+
+        int age = 20;
+        long longNumber = 100000;
+        double doubleWage = 2000;
+        float floatWage = 3000F;
+        byte byteAge = 127;
+        short shortAge = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'w';
+        String curso = "MaratonaJava";
+
+        System.out.println("O melhor curso de Java e: " + curso);
+    }
+}
